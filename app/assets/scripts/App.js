4722a74cf3bd00ddd92cd37f5df059bc7aba5415
Cihan <ugrchnicz@gmail.com>
2022-01-10 22:09:06 +0300

    Completed the reveal on scrolll behaviour.

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,7 +1,17 @@
 import '../styles/styles.css'
 import MobileMenu from './modules/MobileMenu'
+import RevealOnScroll from './modules/RevealOnScroll'
 
 let mobileMenu = new MobileMenu();
+
+const q = new RevealOnScroll(document.querySelectorAll(".feature-item"), 75)
+q;
+const p = new RevealOnScroll(document.querySelectorAll(".testimonial"), 60)
+p;
+
+
+
+
 
 if(module.hot){
     module.hot.accept()
